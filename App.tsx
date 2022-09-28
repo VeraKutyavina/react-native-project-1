1c03b379dc32024772f1cb4d4c3f61639e7907c3
Vera Kutyavina <vera@MacBook-Pro-Vera.local>
2022-09-29 01:55:15 +0300

    homework

--- App.tsx
+++ App.tsx
@@ -1,16 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import {ImageBackground, StyleSheet, Text, View} from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, ScrollView } from 'react-native';
+import Block from "./components/Block";
 const image = require('./back.png');
+const kermit1 = require('./kermit1.jpeg');
+const kermit2 = require('./kermit2.jpeg');
+const kermit3 = require('./kermit3.jpeg');
+
+const BlocksData = [
+  {
+    subtitle: "He was made out of a coat",
+    img: kermit1,
+  },
+  {
+    subtitle: "He wasn't originally a frog",
+    img: kermit2,
+  },
+  {
+    subtitle: "He wasn't the original star of 'The Muppet Show'",
+    img: kermit3,
+  },
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/*<View style={styles.top}>*/}
-
-      {/*</View>*/}
       <ImageBackground source={image} resizeMode="cover" style={styles.top} />
       <View style={styles.content}>
         <Text style={styles.title}>Kermit page</Text>
+        <ScrollView
+          scrollEventThrottle={16}
+        >
+          <View>
+            <Text style={styles.blocksTitle}>
+              What do you know about Kermit?
+            </Text>
+
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              {BlocksData.map(({ subtitle, img }, i) => <Block img={img} subtitle={subtitle} key={i} />)}
+            </ScrollView>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -24,9 +56,18 @@ const styles = StyleSheet.create({
     backgroundColor: "black",
     height: 112,
   },
+  blockTitle: {
+    fontWeight: '700',
+    paddingHorizontal: 16,
+  },
   title: {
     fontSize: 20,
     textTransform: 'uppercase',
+  },
+  blocksTitle: {
+    fontSize: 20,
+    fontWeight: '700',
+    paddingVertical: 20,
   },
   content: {
     padding: 32,
