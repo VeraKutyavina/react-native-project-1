fa52b7d97dff0fa57ddf9a6e96d4b7cf71757eba
Vera Kutyavina <vera.kutiavina@scalewill.com>
2022-11-23 22:06:42 +0300

    start mobx

--- App.tsx
+++ App.tsx
@@ -1,32 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './components/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import UsersListScreen from './components/UsersListScreen';
-import AboutUsScreen from './components/AboutUsScreen';
-import UserProfileScreen from './components/UserProfileScreen';
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+import { makeAutoObservable } from 'mobx'
+import { observer } from 'mobx-react-lite'
 
+class Counter {
+  count = 0
 
-const Stack = createNativeStackNavigator();
+  constructor() {
+    makeAutoObservable(this)
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="UserList" component={UsersListScreen} />
-          <Stack.Screen name="AboutUs" component={AboutUsScreen} />
-          <Stack.Screen name="UserProfile" component={UserProfileScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
-  );
+  increment() {
+    this.count += 1
+  }
+
+  decrement() {
+    this.count -= 1
+  }
+
+  reset() {
+    this.count = 0
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-});
+const counter = new Counter()
+
+const App = observer(() => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>{counter.count}</Text>
+    <Button onPress={() => counter.increment()} title="+" />
+    <Button onPress={() => counter.decrement()} title="-" />
+    <Button onPress={() => counter.reset()} title="Reset" />
+  </View>
+))
+
+export default App
